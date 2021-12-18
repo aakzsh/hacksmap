6cd68865bed93a6c7b6c5eaa5bd8418f347ccd2e
aakzsh <aakashferrari@gmail.com>
2021-12-18 21:32:08 +0530

    add text editing on video

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 import urllib.request
-
+import ffmpeg
+import glob
+from pyffmpeg import FFmpeg
+import urllib.request
+from moviepy.editor import *
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+import numpy as np
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -23,8 +30,180 @@ def wrapped(username):
 
 @app.route('/map/<username>')
 def map(username):
-    return render_template('map.html')
+    return render_template('lol.html')
 
+@app.route('/tryvid')
+def tryvid():
+    ff = FFmpeg()
+    name = "aakash"
+    followers = 19
+    pfpurl = "https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/001/420/227/datas/profile.jpg"
+    registered  = 46
+    bestproj = "ecstatic zanie visualise sab kuch"
+    topprojs = [["url", "name"]]
+    hours = 750
+    totalproj = 37
+    totallikes = 100
+    teammates=27
+    soulmate = "shruti"
+    soulmate_url = "https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/001/698/915/datas/profile.jpg"
+    x = 150
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', x)
+    urllib.request.urlretrieve(soulmate_url, "static/soulmate.png")
+    urllib.request.urlretrieve(pfpurl, "static/photo.png")
+    im = Image.open("static/soulmate.png")
+    im1 = im.resize((400,400))
+    im1.save('static/soulmate1.png')
+    img=Image.open("static/soulmate1.png").convert("RGB")
+    npImage=np.array(img)
+    h,w=img.size
+    alpha = Image.new('L', img.size,0)
+    draw = ImageDraw.Draw(alpha)
+    draw.pieslice([0,0,h,w],0,360,fill=255)
+    npAlpha=np.array(alpha)
+    npImage=np.dstack((npImage,npAlpha))
+    Image.fromarray(npImage).save('static/soulmate2.png')
+
+    im = Image.open("static/photo.png")
+    im1 = im.resize((400,400))
+    im1.save('static/photo1.png')
+    img=Image.open("static/photo1.png").convert("RGB")
+    npImage=np.array(img)
+    h,w=img.size
+    alpha = Image.new('L', img.size,0)
+    draw = ImageDraw.Draw(alpha)
+    draw.pieslice([0,0,h,w],0,360,fill=255)
+    npAlpha=np.array(alpha)
+    npImage=np.dstack((npImage,npAlpha))
+    Image.fromarray(npImage).save('static/photo2.png')
+ 
+
+    # 2
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 150)
+    lol = name.split(" ")[0]
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(lol, font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), lol, ((255,255,245)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    
+    ff.options(f"-i static/parts/2.mp4 -i static/kardolol.png -filter_complex overlay=0:700 static/final/2.mp4")
+    ff.options(f"-i static/final/2.mp4 -i static/photo2.png -filter_complex overlay=350:300 static/final/21.mp4")
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 100)
+    lol = str(followers)
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(str(followers), font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), str(followers), ((255,255,245)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    ff.options(f"-i static/final/21.mp4 -i static/kardolol.png -filter_complex overlay=0:1000 static/final/22.mp4")
+    # 4
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 100)
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    hehe = 140
+    if len(str(registered))>2:
+        hehe = 100
+    elif len(str(registered)) == 1:
+        hehe = 180
+        
+    image_editable.text((30,30), str(registered), ((255,209,0)), font=title_font, )
+    my_image.save('static/kardo.png')
+    
+    ff.options(f"-i static/parts/4.mp4 -i static/kardo.png -filter_complex overlay={hehe-40}:875 static/final/4.mp4")
+    # 6
+    if len(bestproj)>20:
+        bestproj = bestproj[0:20]+".."
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 80)
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(bestproj, font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), str(bestproj), ((255,255,255)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    
+    ff.options(f"-i static/parts/6.mp4 -i static/kardolol.png -filter_complex overlay=0:800 static/final/6.mp4")    
+    # 8
+
+    # 10
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 120)
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(str(hours), font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), str(hours), ((255,255,255)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    
+    ff.options(f"-i static/parts/10.mp4 -i static/kardolol.png -filter_complex overlay=-240:600 static/final/10.mp4")
+
+    # 12
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 180)
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(str(totalproj), font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), str(totalproj), ((255,209,0)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    
+    ff.options(f"-i static/parts/12.mp4 -i static/kardolol.png -filter_complex overlay=0:500 static/final/12.mp4")
+
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(str(totallikes), font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), str(totallikes), ((255,209,0)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    
+    ff.options(f"-i static/final/12.mp4 -i static/kardolol.png -filter_complex overlay=0:1300 static/final/121.mp4")
+    # 14
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 100)
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    hehe = 140
+    if len(str(teammates))>2:
+        hehe = 100
+    elif len(str(teammates)) == 1:
+        hehe = 180
+        
+    image_editable.text((30,30), str(teammates), ((61,76,245)), font=title_font, )
+    my_image.save('static/kardo.png')
+    
+    ff.options(f"-i static/parts/14.mp4 -i static/kardo.png -filter_complex overlay={hehe}:970 static/final/14.mp4")
+    # 16
+    title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 150)
+    lol = soulmate.split(" ")[0]
+    my_image = Image.open("static/bg.png")
+    image_editable = ImageDraw.Draw(my_image)
+    w, h = image_editable.textsize(lol, font=title_font)
+    image_editable.text(((1080-w)/2,(479-h)/2), lol, ((61,76,245)), font=title_font, )
+    my_image.save('static/kardolol.png')
+    
+    ff.options(f"-i static/parts/16.mp4 -i static/kardolol.png -filter_complex overlay=0:900 static/final/16.mp4")
+    ff.options(f"-i static/final/16.mp4 -i static/soulmate2.png -filter_complex overlay=350:400 static/final/161.mp4")
+    vid_1 = VideoFileClip("static/parts/1.mp4")
+    vid_2 = VideoFileClip("static/final/22.mp4")
+    vid_3 = VideoFileClip("static/parts/3.mp4")
+    vid_4 = VideoFileClip("static/final/4.mp4")
+    vid_5 = VideoFileClip("static/parts/5.mp4")
+    vid_6 = VideoFileClip("static/final/6.mp4")
+    vid_7 = VideoFileClip("static/parts/7.mp4")
+    vid_8 = VideoFileClip("static/parts/8.mp4")
+    vid_9 = VideoFileClip("static/parts/9.mp4")
+    vid_10 = VideoFileClip("static/final/10.mp4")
+    vid_11 = VideoFileClip("static/parts/11.mp4")
+    vid_12 = VideoFileClip("static/final/121.mp4")
+    vid_13 = VideoFileClip("static/parts/13.mp4")
+    vid_14 = VideoFileClip("static/final/14.mp4")
+    vid_15 = VideoFileClip("static/parts/15.mp4")
+    vid_16 = VideoFileClip("static/final/161.mp4")
+    vid_17 = VideoFileClip("static/parts/17.mp4")
+    final_video= concatenate_videoclips([vid_1,vid_2,vid_3,vid_4,vid_5,vid_6,vid_7,vid_8,vid_9,vid_10,vid_11,vid_12,vid_13,vid_14,vid_15,vid_16,vid_17])
+
+    final_video.write_videofile("static/final_video.mp4")
+    # clip = VideoFileClip("dsa_geek.mp4")
+# loading audio file
+    # audioclip = AudioFileClip("static/music.mp3").subclip(0, 5)
+    # videoclip = final_video.set_audio(audioclip)
+    # videoclip.write_videofile("static/final_video.mp4")
+  
+
+    return render_template('lol.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
