3c5f8286338b06449195923b025f5cddedcf73b8
aakzsh <aakashferrari@gmail.com>
2021-12-19 09:44:24 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import urllib.request
-import ffmpeg
-import glob
+import glob, os
 from pyffmpeg import FFmpeg
 import urllib.request
 from moviepy.editor import *
@@ -33,6 +32,7 @@ def map(username):
 
 @app.route('/tryvid')
 def tryvid():
+    print("on tryvid path hehe")
     ff = FFmpeg()
     name = "aakash"
     followers = 19
@@ -47,6 +47,7 @@ def tryvid():
     soulmate = "shruti"
     soulmate_url = "https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/001/698/915/datas/profile.jpg"
     x = 150
+    print("getting soulmate image")
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', x)
     urllib.request.urlretrieve(soulmate_url, "static/soulmate.png")
     urllib.request.urlretrieve(pfpurl, "static/photo.png")
@@ -62,7 +63,7 @@ def tryvid():
     npAlpha=np.array(alpha)
     npImage=np.dstack((npImage,npAlpha))
     Image.fromarray(npImage).save('static/soulmate2.png')
-
+    print("getting pfp")
     im = Image.open("static/photo.png")
     im1 = im.resize((400,400))
     im1.save('static/photo1.png')
@@ -76,7 +77,7 @@ def tryvid():
     npImage=np.dstack((npImage,npAlpha))
     Image.fromarray(npImage).save('static/photo2.png')
  
-
+    print("applying edits on 2nd frame")
     # 2
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 150)
     lol = name.split(" ")[0]
@@ -97,6 +98,7 @@ def tryvid():
     my_image.save('static/kardolol.png')
     ff.options(f"-i static/final/21.mp4 -i static/kardolol.png -filter_complex overlay=0:1000 static/final/22.mp4")
     # 4
+    print("applying edits on 4th frame")
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 100)
     my_image = Image.open("static/bg.png")
     image_editable = ImageDraw.Draw(my_image)
@@ -111,6 +113,7 @@ def tryvid():
     
     ff.options(f"-i static/parts/4.mp4 -i static/kardo.png -filter_complex overlay={hehe-40}:875 static/final/4.mp4")
     # 6
+    print("applying edits on 6th frame")
     if len(bestproj)>20:
         bestproj = bestproj[0:20]+".."
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 80)
@@ -124,6 +127,7 @@ def tryvid():
     # 8
 
     # 10
+    print("applying edits on 10th frame")
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 120)
     my_image = Image.open("static/bg.png")
     image_editable = ImageDraw.Draw(my_image)
@@ -134,6 +138,7 @@ def tryvid():
     ff.options(f"-i static/parts/10.mp4 -i static/kardolol.png -filter_complex overlay=-240:600 static/final/10.mp4")
 
     # 12
+    print("applying edits on 12th frame")
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 180)
     my_image = Image.open("static/bg.png")
     image_editable = ImageDraw.Draw(my_image)
@@ -151,6 +156,7 @@ def tryvid():
     
     ff.options(f"-i static/final/12.mp4 -i static/kardolol.png -filter_complex overlay=0:1300 static/final/121.mp4")
     # 14
+    print("applying edits on 14th frame")
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 100)
     my_image = Image.open("static/bg.png")
     image_editable = ImageDraw.Draw(my_image)
@@ -165,6 +171,7 @@ def tryvid():
     
     ff.options(f"-i static/parts/14.mp4 -i static/kardo.png -filter_complex overlay={hehe}:970 static/final/14.mp4")
     # 16
+    print("applying edits on 16th frame")
     title_font = ImageFont.truetype('static/Montserrat-ExtraBold.ttf', 150)
     lol = soulmate.split(" ")[0]
     my_image = Image.open("static/bg.png")
@@ -175,6 +182,8 @@ def tryvid():
     
     ff.options(f"-i static/parts/16.mp4 -i static/kardolol.png -filter_complex overlay=0:900 static/final/16.mp4")
     ff.options(f"-i static/final/16.mp4 -i static/soulmate2.png -filter_complex overlay=350:400 static/final/161.mp4")
+
+    print("defining vids baamzi for merge")
     vid_1 = VideoFileClip("static/parts/1.mp4")
     vid_2 = VideoFileClip("static/final/22.mp4")
     vid_3 = VideoFileClip("static/parts/3.mp4")
@@ -192,16 +201,19 @@ def tryvid():
     vid_15 = VideoFileClip("static/parts/15.mp4")
     vid_16 = VideoFileClip("static/final/161.mp4")
     vid_17 = VideoFileClip("static/parts/17.mp4")
+
+    print("starting merge in a sec")
     final_video= concatenate_videoclips([vid_1,vid_2,vid_3,vid_4,vid_5,vid_6,vid_7,vid_8,vid_9,vid_10,vid_11,vid_12,vid_13,vid_14,vid_15,vid_16,vid_17])
-
+    print("writing final vid file")
     final_video.write_videofile("static/final_video.mp4")
-    # clip = VideoFileClip("dsa_geek.mp4")
-# loading audio file
-    # audioclip = AudioFileClip("static/music.mp3").subclip(0, 5)
-    # videoclip = final_video.set_audio(audioclip)
-    # videoclip.write_videofile("static/final_video.mp4")
-  
 
+    print("doneeeeeeeeeee")
+
+    print("removing useless file")
+    dir = 'static/final/'
+    filelist = glob.glob(os.path.join(dir, "*"))
+    for f in filelist:
+        os.remove(f)
     return render_template('lol.html')
 
 if __name__ == "__main__":
