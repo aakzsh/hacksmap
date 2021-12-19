4313cbf8e85f9d63c06140847f197e2282febf4e
Atharv-777 <63581644+Atharv-777@users.noreply.github.com>
2021-12-19 17:51:33 +0530

    Update map.py

--- map.py
+++ map.py
@@ -2,4 +2,7 @@ def locateHacker(city_address):
     from geopy.geocoders import Nominatim
     g = Nominatim(user_agent="hacksmap")
     location = g.geocode(city_address)
-    return location.latitude,location.longitude
+    lst = []
+    lst.append(location.latitude)
+    lst.append(location.longitude)
+    return lst
